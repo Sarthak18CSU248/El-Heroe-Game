125154547a501bb89675f89f9e22a057d24de8be
Sarthak18CSU248 <57385445+Sarthak18CSU248@users.noreply.github.com>
2020-04-13 17:47:08 +0530

    Commit- 13/04

---
+++ Assets/Scripts/GamePlay/OtherWorld.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherWorld : MonoBehaviour
+{
+
+    public GameObject LoadButton;
+    private void OnTriggerEnter(Collider target)
+    {
+        if(target.tag=="Player")
+        {
+            LoadButton.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider target)
+    {
+        if (target.tag == "Player")
+        {
+            LoadButton.SetActive(false);
+        }
+    }
+
+
+}//class
