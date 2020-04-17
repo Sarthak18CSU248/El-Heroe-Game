66bd5a9e218b3917e6784a49977bf51527aaf07f
Sarthak18CSU248 <57385445+Sarthak18CSU248@users.noreply.github.com>
2020-04-17 15:12:03 +0530

    Commmit-17/04

---
+++ Assets/Scripts/Player Scripts/PlayerStatus.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatus : MonoBehaviour
+{
+    public GameObject[] PlayerSwords;
+
+    private GameObject SwordPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        GameObject[] SwordChangeButton = GameObject.FindGameObjectsWithTag("SwordBtn");
+        foreach (GameObject btn in SwordChangeButton)
+        {
+            btn.GetComponent<Button>().onClick.AddListener(ChangeSword);
+        }
+        SwordPanel = GameObject.Find("Sword Panel");
+        //SwordPanel.SetActive(false);
+        //GameObject.Find("Sword Scroller").SetActive(false);
+        GameObject.Find("Sword Button").GetComponent<Button>().onClick.AddListener(ActivateSwordPanel);
+    }
+
+    public void ActivateSwordPanel()
+    {
+        if(SwordPanel.activeInHierarchy)
+        {
+            SwordPanel.SetActive(false);
+            GameObject.Find("Sword Scroller").SetActive(false);
+        }
+        else
+        {
+            SwordPanel.SetActive(true);
+            //GameObject.Find("Sword Scroller").SetActive(true);
+        }
+    }
+    public void ChangeSword()
+    {
+        int swordIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+        for (int i=0;i<PlayerSwords.Length;i++)
+        {
+            PlayerSwords[i].SetActive(false);
+        }
+        PlayerSwords[swordIndex].SetActive(true); 
+    }
+    
+
+}//class
---
+++ Assets/Shop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public GameObject ShopPanel,SwordPanel;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ActivateInventory()
+    {
+        if (ShopPanel.activeInHierarchy)
+        {
+            ShopPanel.SetActive(false);
+        }
+        else
+        {
+            ShopPanel.SetActive(true);
+            SwordPanel.SetActive(false);
+        }
+    }
+}
