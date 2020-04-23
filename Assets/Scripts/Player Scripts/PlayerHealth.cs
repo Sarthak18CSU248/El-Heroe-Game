c79214ac4d2a1053c87d773fcbad285fa0a00ccf
Sarthak18CSU248 <57385445+Sarthak18CSU248@users.noreply.github.com>
2020-04-23 20:15:22 +0530

    Commit-23/04

--- Assets/Scripts/Player Scripts/PlayerHealth.cs
+++ Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float health = 100f;
         public GameObject deadFX;
+    public Slider player_health;
 
+    private void Start()
+    {
+        player_health = GameObject.Find("HealthSlider").GetComponent<Slider>();
+    }
     public void TakeDamage(float damageAmount)
     {
+       
         health -= damageAmount;
+        player_health.value -= damageAmount * 0.01f;
         Debug.Log("PlayerHealth");
         if(health <=0)
         {
