6b3b4b27e2fd97b0ca378536dca609657e730c31
Sarthak18CSU248 <57385445+Sarthak18CSU248@users.noreply.github.com>
2020-04-23 20:39:12 +0530

    Commit-23/04

--- Assets/Scripts/GamePlay/Bridge_Transit.cs
+++ Assets/Scripts/GamePlay/Bridge_Transit.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bridge_Transit : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
         {
--- Assets/Scripts/GamePlay/Village_Transit.cs
+++ Assets/Scripts/GamePlay/Village_Transit.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class Village_Transit : MonoBehaviour
 {
     
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if(other.tag == "Player")
         {
             other.transform.position = new Vector3(43.41804f,0.9521687f,90.66909f); 
         }
     }
-
+    
 
     //other.transform.position = new Vector3(44.31171f,10.95034f,112.2168f);
 }
