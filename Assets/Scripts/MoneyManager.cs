106d3ed20c49bbc02e0a673366dd456e1d01af09
Sarthak18CSU248 <57385445+Sarthak18CSU248@users.noreply.github.com>
2020-04-30 19:51:46 +0530

    Shop System

---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyManager : MonoBehaviour
+{
+    public static MoneyManager instance;
+    private Text money;
+    private int currentCoins = 20000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        money = gameObject.GetComponent<Text>();
+        money.text = "$" + Convert.ToString(currentCoins);
+    }
+
+    public void changeMonetaryStatus(int expense)
+    {
+        int current_target = currentCoins - expense;
+        StartCoroutine(CoinDecrease(current_target));
+    }
+   
+    IEnumerator CoinDecrease(int coins_target)
+    {
+        while (currentCoins != coins_target)
+        {
+            if (currentCoins == coins_target)
+                break;
+            else
+            {
+                currentCoins -= 1;
+                yield return new WaitForSeconds(1f);
+                money.text = "$"+Convert.ToString(currentCoins);
+
+            }
+        }
+    }
+}
