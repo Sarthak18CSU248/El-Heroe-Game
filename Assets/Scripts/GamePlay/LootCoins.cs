5653315e5fec7002508474e2de7e1178684b223a
Sarthak18CSU248 <57385445+Sarthak18CSU248@users.noreply.github.com>
2020-05-02 21:16:50 +0530

    Treasure Chest

---
+++ Assets/Scripts/GamePlay/LootCoins.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootCoins : MonoBehaviour
+{
+    [SerializeField] private Mesh chest_open;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag=="Player")
+        {
+            gameObject.GetComponent<MeshFilter>().mesh = chest_open;
+        }
+    }
+}
